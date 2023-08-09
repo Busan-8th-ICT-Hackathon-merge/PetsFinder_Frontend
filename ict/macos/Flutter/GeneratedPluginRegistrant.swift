ced085a05a75c6e6ba7511cebac59e565b126082
kbs1027 <rlaqjatn1027@gmail.com>
2023-08-09 19:57:59 +0900

    input_map

--- ict/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ict/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import shared_preferences_foundation
+import geolocator_apple
+import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
